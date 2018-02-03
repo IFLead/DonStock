fd0dc97e39cdb4f66c5eb4a7a85c66ca44e1fef8
macheteya <macheteya@yandex.ru>
2018-02-03 23:24:29 +0300

    modal_category

--- src/App.vue
+++ src/App.vue
@@ -468,92 +468,92 @@
           width: 100%
           max-height: 348px
 
-      p.tagline
-        font-size: 1.875em //30px
-        //line-height: 40px
-        color: black
-        text-align: center
-        padding-top: 5.629139% // 51px / 906 px
-        padding-bottom: 6.622517% // 60px / 906px
-        margin-bottom: 0px
-        @include media-breakpoint-down(sm)
-          padding-left: 5%
-          padding-right: 5%
-          font-size: 1.6em
-        @include media-breakpoint-down(xs)
-          font-size: 1em
-
-      .form-group
-        padding-left: 26.600442% //241px / 906px
-        padding-right: 26.600442% //241px / 906px
-        margin: 0px
-        padding-bottom: 7.3951435% // 67px / 906px
-        @include media-breakpoint-down(xs)
-          padding-left: 15%
-          padding-right: 15%
-
-        select
-          max-width: 426px
-          border-radius: 0px
-          background: transparent url("./assets/combobox_category.png") no-repeat right
-          appearance: none
-
-        .form-control
-          width: 100%
-          height: 39.5px
-          padding: 0px
-          font-size: 1.5em
-          color: #C5C5C5
-          border-bottom: 1px solid black
-          border-top: none
-          border-left: none
-          border-right: none
-          @include media-breakpoint-down(xs)
-            font-size: 0.9em
-
-          option
-            font-size: 1em //24px
-            min-height: 1.5em //32px
-            border: 1px solid #95989A
+          p.tagline
+            font-size: 1.875em //30px
+            //line-height: 40px
             color: black
+            text-align: center
+            padding-top: 5.629139% // 51px / 906 px
+            padding-bottom: 6.622517% // 60px / 906px
+            margin-bottom: 0px
+            @include media-breakpoint-down(sm)
+              padding-left: 5%
+              padding-right: 5%
+              font-size: 1.6em
+            @include media-breakpoint-down(xs)
+              font-size: 1em
 
-          option:checked
-            background-color: #1DB954
+          .form-group
+            padding-left: 26.600442% //241px / 906px
+            padding-right: 26.600442% //241px / 906px
+            margin: 0px
+            padding-bottom: 7.3951435% // 67px / 906px
+            @include media-breakpoint-down(xs)
+              padding-left: 15%
+              padding-right: 15%
 
-          option:hover
-            background-color: #1DB954
+            select
+              max-width: 426px
+              border-radius: 0px
+              background: transparent url("./assets/combobox_category.png") no-repeat right
+              appearance: none
 
-      .back-button
-        float: left
-        //width: 50px
-        // height: 50px
-        padding-left: 9.052% // 82px / 908px = 9.030837%
-        padding-bottom: 3.754% // 34px / 908px = 3.744493%
+            .form-control
+              width: 100%
+              height: 39.5px
+              padding: 0px
+              font-size: 1.5em
+              color: #C5C5C5
+              border-bottom: 1px solid black
+              border-top: none
+              border-left: none
+              border-right: none
+              @include media-breakpoint-down(xs)
+                font-size: 0.9em
 
-        img
-          width: 50px
-          height: 50px
-          @include media-breakpoint-down(xs)
-            width: 30px
-            height: 30px
+              option
+                font-size: 1em //24px
+                min-height: 1.5em //32px
+                border: 1px solid #95989A
+                color: black
 
-      .green-btn
-        width: 30.023% // 260px / 908px = 28.634361%
-        height: 60px
-        border-radius: 35px
-        background-color: $green
-        color: white
-        float: right
-        margin-bottom: 3.312% // 30px / 908px = 0.03303965
-        margin-right: 3.422% // 31px / 908px = 0.03414097
-        font-size: 1.875em // 30px
-        @include media-breakpoint-down(sm)
-          font-size: 1.5em
-          width: 35%
-        @include media-breakpoint-down(xs)
-          width: 50%
-          height: 30px
-          font-size: 1em
+              option:checked
+                background-color: #1DB954
+
+              option:hover
+                background-color: #1DB954
+
+          .back-button
+            float: left
+            //width: 50px
+            // height: 50px
+            padding-left: 9.052% // 82px / 908px = 9.030837%
+            padding-bottom: 3.754% // 34px / 908px = 3.744493%
+
+            img
+              width: 50px
+              height: 50px
+              @include media-breakpoint-down(xs)
+                width: 30px
+                height: 30px
+
+          .green-btn
+            width: 30.023% // 260px / 908px = 28.634361%
+            height: 60px
+            border-radius: 35px
+            background-color: $green
+            color: white
+            float: right
+            margin-bottom: 3.312% // 30px / 908px = 0.03303965
+            margin-right: 3.422% // 31px / 908px = 0.03414097
+            font-size: 1.875em // 30px
+            @include media-breakpoint-down(sm)
+              font-size: 1.5em
+              width: 35%
+            @include media-breakpoint-down(xs)
+              width: 50%
+              height: 30px
+              font-size: 1em
 
 
 
